d9757c304ff2113f98e5e949b1fb2e3140136791
pcfx <pcfx@qq.com>
2025-02-14 15:35:40 +0000

    Site updated: 2025-02-14 15:35:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/21/git使用/","2025/02/12/hello-world/","2025/02/07/笔记本780M部署deepseek-r1指北/"];function toRandomPost(){
+var posts=["2025/01/21/git使用/","2025/02/07/笔记本780M部署deepseek-r1指北/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
