a171eb61069a2a0b3e2852ddead67d42fc7ea121
pcfx <pcfx@qq.com>
2025-03-30 05:10:00 +0000

    Site updated: 2025-03-30 05:10:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/04/GDB学习指北/","2025/03/07/Markdown学习指北/","2025/01/21/git使用/","2025/02/07/笔记本780M部署deepseek-r1指北/"];function toRandomPost(){
+var posts=["2025/03/04/GDB学习指北/","2025/01/21/git使用/","2025/02/07/笔记本780M部署deepseek-r1指北/","2025/03/07/Markdown学习指北/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
