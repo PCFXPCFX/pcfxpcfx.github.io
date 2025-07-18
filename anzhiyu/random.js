3e3f6ac5c78d88c4830be5b40165bdb5b0139d83
pcfx <pcfx@qq.com>
2025-07-18 16:57:52 +0800

    Site updated: YYYY-07-Jul 18, 2025 16:57:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/22/git使用指北/","2025/02/08/笔记本780M部署deepseek-r16b指北/","2025/03/04/GDB学习指北/","2025/03/08/Markdown学习指北/","2025/05/10/双指针，滑动窗口/"];function toRandomPost(){
+var posts=["2025/03/08/Markdown学习指北/","2025/05/10/双指针，滑动窗口/","2025/03/04/GDB学习指北/","2025/01/22/git使用指北/","2025/02/08/笔记本780M部署deepseek-r16b指北/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
